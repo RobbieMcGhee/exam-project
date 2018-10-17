d230614afffde381d6264d3239d995b2b571bf76
Robert McGhee <robertmcghee@Roberts-MacBook-Air.local>
2018-10-17 13:17:35 +0200

    when logging in successfully you are now redirected to filtering page

--- exam1.js
+++ exam1.js
@@ -24,11 +24,10 @@ const user = [
  }
  
 var users = [];
-// Fill it up with a few users
+// Here we add some New Users to the empty user array 
 users.push(new User("jeppe@gmail.com", "Jeppe Bangskjær", "Kastrupvej 1", "2770", "Kastrup", "Jep", "Jep"));
 users.push(new User("frederik@gmail.com", "Frederik Mouritsen", "Kastrupvej 2", "2770", "Kastrup", "Fre", "Fre")); 
 users.push(new User("robert@gmail.com", "Robert McGhee", "Kastrupvej 3", "2770", "Kastrup", "Rob", "Rob"));    
-
 
 //Existing Users signing in:
 document.getElementById("signIn").addEventListener("click", function (){ // This negates the "required" function in HTML! 
@@ -49,7 +48,8 @@ document.getElementById("signIn").addEventListener("click", function (){ // This
     for (var i = 0; i < users.length; i++) {
         if (eMail == users[i].email && password == users[i].pw) {
             document.getElementById('signInResult').textContent = 'You have succesfully signed in';
-            return true;
+            return window.location.replace("/Users/robertmcghee/Desktop/programmingExamProject/exam-project/filterPage.html");
+            
         }
         else{
             document.getElementById('signInResult').textContent = 'Incorrect Username or Password';
@@ -60,7 +60,7 @@ document.getElementById("signIn").addEventListener("click", function (){ // This
 document.getElementById("register").addEventListener("click",function(){
     console.log('The Register button is clicked!');
     
-    var form = document.getElementById('registerForm'); // Gave the form this id!     
+    var form = document.getElementById('registerForm'); // Same as in sign in, the addEvent function is negating the required function, so we add the loop to check if the fields are empty!    
 
     for(var i=0; i < form.elements.length; i++){
         if(form.elements[i].value === '' && form.elements[i].hasAttribute('required')){
@@ -68,7 +68,6 @@ document.getElementById("register").addEventListener("click",function(){
             return false;
         }
     }
-
 
     var registerEmail = document.getElementById('registerEmail').value;
     var registerStreetName = document.getElementById('registerStreetName').value;
@@ -81,13 +80,12 @@ document.getElementById("register").addEventListener("click",function(){
     function valEmail (mail) { //This will check if the input in the email fields match the requirements. 
         return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(mail)
     }
-/* Now, after the new users have entered the information, we need to make sure it does not
-interfer with the existing user data*/
-// New Users Registering: 
 
+// We need to make sure new user info does not interfer with the existing user data
+// New Users Registering: 
     var newerUser = new User(registerEmail, registerFullName, registerStreetName, registerZipCode, registerCity, registerPassword, repeatPassword);
   //  var re = /^((?!(0))[0-9]{4})$/g;  // tried to use regex to check the pattern of zip codes, does not work tho! 
-    for (var i = 0; i < users.length; i++) {     //Reversing the logic here could make sense!'
+    for (var i = 0; i < users.length; i++) {  //Reversing the logic here could make sense!'
     console.log("for loop called")
     
         
@@ -104,6 +102,8 @@ interfer with the existing user data*/
             };
     };
 });
+
+// Re-directing to the HTML with the filtering function. 
 
 
 
