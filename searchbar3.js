ae3a6d968824e884229244a468e813527d619055
RobbieMcGhee <43065909+RobbieMcGhee@users.noreply.github.com>
2018-11-01 14:50:19 +0100

    Update searchbar3.js

--- searchbar3.js
+++ searchbar3.js
@@ -4,207 +4,363 @@
 we can make a class of them in the JS file.*/
 class Lamp {
 
-    constructor(type, color, price, imagePath) {      
+    constructor(type, color, price, imagePath, movie, character) { // TESTING movie & character      
       this.type = type;
       this.color = color;
       this.price = price;
-      this.imagePath = imagePath;
-
+      this.imagePath = imagePath; 
+      if (!movie) {
+        this.movie = ""; 
+      } else {
+        this.movie = movie; // TESTING
+      }
+      if (!character) {
+        this.character = ""; 
+      } else {
+        this.character = character; // TESTING
+      }
     }
 
     createHTML(){
-        return "<li class='collection-item'><div class='image'><img src='" + // Should we add the Collection header?
+        return "<li class='collection-item'><div class='image'><img src='" + 
         this.imagePath + "' width='200px' height='200px' /></div><a href='#'>" + 
         this.type + " </a><div class='color'>" + 
         this.color + "</div><div class='price'>" + 
+        this.movie + "</div><div class='movie'>" +  // TESTING
+        this.character + "</div><div class='character'>" + // TESTING     
         this.price + "</div><div class='button'><button class='addToCart'>Add to Cart</button></li>";
     }
  }
 
 var lamps = [];
 // Here we add some New lamps to the empty lamps array 
-lamps.push(new Lamp(
-    "Ceiling Lamp", 
-    "Red",
-    "399", 
-    "Red Ceiling Lamp.jpg")
-    );
+
 lamps.push(new Lamp(
     "Ceiling Lamp", 
     "Blue", 
     "199",
-    "Blue Ceiling Lamp.jpg")
-    );
-lamps.push(new Lamp(
-    "Ceiling Lamp", 
-    "Green", 
-    "599",
-    "Green Ceiling Lamp.jpg")
-    );
-lamps.push(new Lamp(
-    "Ceiling Lamp", 
-    "White", 
-    "299",
-    "White Ceiling Lamp.jpg")
-    );
-lamps.push(new Lamp(
-    "Ceiling Lamp", 
-    "Steel", 
-    "899",
-    "Steel Ceiling Lamp.jpg")
-    );
-lamps.push(new Lamp(
-    "Ceiling Lamp", 
-    "Rainbow", 
-    "1399",
-    "Rainbow Ceiling Lamp.jpg")
-    );
-lamps.push(new Lamp(
-    "Ceiling Lamp", 
-    "Orange", 
-    "399",
-    "Orange Ceiling Lamp.jpg")
-    );
-lamps.push(new Lamp(
-    "Ceiling Lamp", 
-    "Black",
-    "399",
-    "Black Ceiling Lamp.jpg")
+    "Spiderman Ceiling.jpeg",
+    "Spiderman",
+    "Marvel")
     );
 lamps.push(new Lamp(
     "Ceiling Lamp", 
     "Yellow", 
-    "99",
-    "Yellow Ceiling Lamp.jpg")
+    "599",
+    "beauty and beast 3 desk.jpg",
+    "Beauty and the beast",
+    "Disney")
     );
 lamps.push(new Lamp(
     "Ceiling Lamp", 
-    "Grey", 
+    "Blue", 
+    "299",
+    "Captain America Ceiling.jpg",
+    "Captain America",
+    "Marvel")
+    );
+lamps.push(new Lamp(
+    "Ceiling Lamp", 
+    "Blue", 
+    "299",
+    "Find Nemo Ceiling.jpeg",
+    "Finding Nemo",
+    "Disney")
+    );
+lamps.push(new Lamp(
+    "Ceiling Lamp", 
+    "Red", 
+    "1399",
+    "Cars ceiling.jpg",
+    "Cars",
+    "Pixar")
+    );
+lamps.push(new Lamp(
+    "Ceiling Lamp", 
+    "Blue", 
+    "399",
+    "Frozen Ceiling.jpg",
+    "Frozen",
+    "Disney")
+    );
+lamps.push(new Lamp(
+    "Ceiling Lamp", 
+    "Green",
     "499",
-    "Grey Ceiling Lamp.jpg")
+    "Hulk and Ironman Ceiling.jpeg",
+    "Hulk",
+    "Avengers")
+    );
+lamps.push(new Lamp(
+    "Ceiling Lamp", 
+    "Pink", 
+    "399",
+    "Minnie Mouse Ceiling.jpg",
+    "Minnie Mouse",
+    "Disney")
     );
 lamps.push(new Lamp(
     "Ceiling Lamp", 
     "Pink", 
     "599",
-    "Pink Ceiling Lamp.jpg")
+    "Minnie Mouse Ceiling.jpeg",
+    "Minnie Mouse",
+    "Disney")
     );
 lamps.push(new Lamp(
     "Ceiling Lamp", 
     "Blue", 
-    "299",
-    "Blue Ceiling lamp 2.jpeg")
-    );
-lamps.push(new Lamp(
-    "Ceiling Lamp", 
-    "Black", 
-    "699",
-    "Black Ceiling Lamp 2.jpg")
-    );
-lamps.push(new Lamp(
-    "Ceiling Lamp", 
-    "Red", 
-    "699",
-    "Ceiling Lamp 2.jpg")
-    );
-lamps.push(new Lamp(
-    "Ceiling Lamp", 
-    "Purple", 
     "499",
-    "Purple Ceiling Lamp.jpg")
+    "Mickey Mouse Ceiling.jpg",
+    "Mickey Mouse",
+    "Disney")
     );
-
-
+lamps.push(new Lamp(
+    "Ceiling Lamp", 
+    "Yellow", 
+    "299",
+    "Pluto Ceiling.jpg",
+    "Pluto",
+    "Disney")
+    );
+lamps.push(new Lamp(
+    "Ceiling Lamp", 
+    "Green", 
+    "699",
+    "Simba Ceiling.jpg",
+    "Simba",
+    "Disney")
+    );
+lamps.push(new Lamp(
+    "Ceiling Lamp", 
+    "Yellow", 
+    "699",
+    "Batman Ceiling.jpg",
+    "Batman",
+    "DC Comics")
+    );
+    
+    // DESK LAMPS! 
     lamps.push(new Lamp(
         "Desk Lamp", 
-        "Red",
-        "399", 
-        "https://www.eurway.com/Shared/Images/Product/Alp-Ceiling-Lamp/alp-ceiling-lamp.png?bw=75&bh=75")
+        "Red", 
+        "499",
+        "Wonderwoman desk.jpg",
+        "Wonder Woman",
+        "DC Comics")
         );
     lamps.push(new Lamp(
         "Desk Lamp", 
-        "Blue", 
+        "Green",
+        "399", 
+        "Batman Bottle.jpg",
+        "Batman", 
+        "DC Comics",) 
+        );
+
+    lamps.push(new Lamp(
+        "Desk Lamp", 
+        "Black",
+        "999", 
+        "Batman Signal.jpg",
+        "Batman",
+        "DC Comics")
+        );
+
+    lamps.push(new Lamp(
+        "Desk Lamp", 
+        "Grey", 
+        "699",
+        "Batman Statue.jpg",
+        "Batman",
+        "DC Comics")
+        );
+    lamps.push(new Lamp(
+        "Desk Lamp",
+        "Red", 
+        "599",
+        "Beauty and beast rose desk.jpeg",
+        "Beauty and Beast",
+        "Disney")
+        );
+    lamps.push(new Lamp(
+        "Desk Lamp",
+        "Gold", 
+        "399",
+        "Beauty and the beast - Lumiere .jpg",
+        "Beauty and Beast",
+        "Disney")
+        );
+    lamps.push(new Lamp(
+        "Desk Lamp", 
+        "Red", 
+        "899",
+        "Deadpool 2 desk.jpeg",
+        "Deadpool",
+        "Marvel")
+        );
+    lamps.push(new Lamp(
+        "Desk Lamp",
+        "Red", 
+        "599",
+        "Deadpool desk.jpeg",
+        "Deadpool",
+        "Marvel")
+        );
+    lamps.push(new Lamp(
+        "Desk Lamp",
+        "Orange", 
+        "999",
+        "Dragon Ball Son Goku.jpeg",
+        "Dragon Ball",
+        "Anime")
+        );
+    lamps.push(new Lamp(
+        "Desk Lamp",
+        "Green",
+        "499",
+        "Dumbo.jpg",
+        "Dumbo",
+        "Disney")
+        );
+    lamps.push(new Lamp(
+        "Desk Lamp", 
+        "Purple", 
         "199",
-        "LINK")
+        "Frozen.jpg",
+        "Frozen",
+        "Disney")
+        );
+    lamps.push(new Lamp(
+        "Desk Lamp",
+        "Blue", 
+        "499",
+        "Goofy.jpg",
+        "Goofy",
+        "Disney")
         );
     lamps.push(new Lamp(
         "Desk Lamp",
         "Green", 
         "599",
-        "LINK")
+        "Hulk Desk .jpg",
+        "Hulk",
+        "Marvel")
         );
     lamps.push(new Lamp(
         "Desk Lamp",
-        "White", 
-        "299",
-        "LINK")
-        );
-    lamps.push(new Lamp(
-        "Desk Lamp", 
-        "Steel", 
-        "899",
-        "LINK")
+        "Red", 
+        "799",
+        "Iron Man Desk.jpg",
+        "Ironman",
+        "Marvel")
         );
     lamps.push(new Lamp(
         "Desk Lamp",
-        "Rainbow", 
-        "1399",
-        "LINK")
-        );
-    lamps.push(new Lamp(
-        "Desk Lamp",
-        "Orange", 
-        "399",
-        "LINK")
-        );
-    lamps.push(new Lamp(
-        "Desk Lamp",
-        "Black",
-        "399",
-        "LINK")
-        );
-    lamps.push(new Lamp(
-        "Desk Lamp", 
         "Yellow", 
-        "99",
-        "LINK")
-        );
-    lamps.push(new Lamp(
-        "Desk Lamp",
-        "Grey", 
-        "499",
-        "LINK")
-        );
-    lamps.push(new Lamp(
-        "Desk Lamp",
-        "Glass", 
-        "599",
-        "LINK")
-        );
-    lamps.push(new Lamp(
-        "Desk Lamp",
-        "Blue", 
-        "299",
-        "LINK")
-        );
-    lamps.push(new Lamp(
-        "Desk Lamp",
-        "Black", 
         "699",
-        "LINK")
+        "Lion King - Simba Cub.jpg",
+        "The Lion King",
+        "Disney")
         );
     lamps.push(new Lamp(
         "Desk Lamp",
         "Red", 
         "699",
-        "LINK")
+        "Mickey Mouse.jpg",
+        "Mickey Mouse",
+        "Disney")
         );
     lamps.push(new Lamp(
         "Desk Lamp",
-        "Steel", 
+        "Yellow", 
         "499",
-        "LINK")
+        "Minions.jpg",
+        "Minions",
+        "Pixar")
         );
+    lamps.push(new Lamp(
+        "Desk Lamp",
+        "Pink", 
+        "399",
+        "Minnie Mouse.jpg",
+        "Minnie Mouse",
+        "Disney")
+        );
+    lamps.push(new Lamp(
+        "Desk Lamp",
+        "Pink", 
+        "459",
+        "Sleeping beauty desk.jpg",
+        "Sleeping Beauty",
+        "Disney")
+        );
+    lamps.push(new Lamp(
+        "Desk Lamp",
+        "Black", 
+        "799",
+        "Sleeping beauty 2 desk.jpg",
+        "Sleeping Beauty",
+        "Disney")
+        );
+
+    lamps.push(new Lamp(
+        "Desk Lamp",
+        "Pink", 
+        "459",
+        "Sleeping beauty desk.jpg",
+        "Sleeping Beauty",
+        "Disney")
+        );
+    lamps.push(new Lamp(
+        "Desk Lamp",
+        "Yellow", 
+        "479",
+        "Snow white 2 desk.jpg",
+        "Snow White",
+        "Disney")
+        );
+    lamps.push(new Lamp(
+        "Desk Lamp",
+        "Pink", 
+        "459",
+        "Snow White desk.jpg",
+        "Snow White",
+        "Disney")
+        );
+    lamps.push(new Lamp(
+        "Desk Lamp",
+        "Blue", 
+        "259",
+        "Superman .jpg",
+        "Superman",
+        "DC Comics")
+        );
+    lamps.push(new Lamp(
+        "Desk Lamp",
+        "Yellow", 
+        "559",
+        "Superman.jpg",
+        "Superman",
+        "DC Comics")
+        );
+    lamps.push(new Lamp(
+        "Desk Lamp",
+        "Red", 
+        "359",
+        "Thor.jpg",
+        "Thor",
+        "Marvel")
+        );
+    lamps.push(new Lamp(
+        "Desk Lamp",
+        "Yellow", 
+        "359",
+        "Winnie the Pooh.jpg",
+        "Winnie the Pooh",
+        "Disney")
+        );
+    
 
 
 lamps.push(new Lamp(
@@ -298,7 +454,7 @@ lamps.push(new Lamp(
     "LINK")
     );
 
-
+ // THis is to display the Lamp array as soon as we reach the page
     var content = ""
 
     for (var i = 0; i < lamps.length; i++) {
@@ -319,7 +475,9 @@ filterInput.addEventListener('keyup', function(){
     var html = "";
 
     for (var i = 0; i < lamps.length; i++) {
-        if (lamps[i].type.indexOf(filterValue) > -1 || lamps[i].color.indexOf(filterValue) > -1 || lamps[i].price.indexOf(filterValue) > -1) {
+        if (lamps[i].type.indexOf(filterValue) > -1 || lamps[i].color.indexOf(filterValue) > -1 || lamps[i].price.indexOf(filterValue) > -1
+            || lamps[i].movie.indexOf(filterValue) > -1 || lamps[i].character.indexOf(filterValue) > -1 
+        ) {
             html += lamps[i].createHTML();
         } else {
             continue;
